d9d1b4030a1a4f80d4c0d2c57b6a60bf1707af8d
paulzfm <zhufengminpaul@163.com>
2015-07-29 23:15:20 +0800

    debugging...

--- util.cpp
+++ util.cpp
@@ -79,7 +79,7 @@ void xwindow_show(Body *bodies)
     XClearArea(display, window, 0, 0, len_window, len_window, 0);
     XSetForeground(display, gc, BlackPixel(display, screen));
     int i, x, y;
-	printf("drawing (%lf, %lf) -> (%d, %d)\n", bodies[0].x, bodies[0].y
+	printf("drawing (%lf, %lf) -> (%d, %d)\n", bodies[0].x, bodies[0].y,
 		(int)(((float)bodies[0].x - xmin) / len_axis * (float)len_window),
 		(int)(((float)bodies[0].y - ymin) / len_axis * (float)len_window)
 	);
