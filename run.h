f02c43c6863a46dcae174b3dcbce11317269f6e7
paulzfm <zhufengminpaul@163.com>
2015-07-29 22:22:01 +0800

    debugging...

--- run.h
+++ run.h
@@ -3,16 +3,19 @@
 
 #include "util.h"
 
-typedef struct
+typedef struct TaskParam_t
 {
     int start, end; // [start, end)
     Body *bodies, *new_bodies;
 } TaskParam;
 
+extern "C" {
+
 // pthread run
-extern "C" void run_pthread_version(int i, int num_threads, Body *bodies,
+void run_pthread_version(int i, int num_threads, Body *bodies,
     Body *new_bodies, float *elapsed_time);
 
 // cuda run
+}
 
 #endif // NBODY_RUN_H_
