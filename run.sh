092693b0ad91b9281e0fc2606032ed784944b4ab
paulzfm <zhufengminpaul@163.com>
2015-07-30 14:54:51 +0800

    update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-./nbody 12 1 1000 0.5 test1.txt no enable -0.3 -0.3 0.6 600
-# ./nbody 12 1 1 0.5 test1.txt no enable -0.3 -0.3 0.6 600
+# ./nbody 12 1 1000 0.5 test1.txt no enable -0.3 -0.3 0.6 600
+./a.out 12 1 1000 0.01 test2.txt 1 enable 0 -20 1 500
