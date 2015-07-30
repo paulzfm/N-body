199e422df636320b983991093b6ee2a7d8bb7c85
paulzfm <zhufengminpaul@163.com>
2015-07-30 20:14:54 +0800

    add QuadTree

--- QuadTree.cpp
+++ QuadTree.cpp
@@ -1,44 +1,129 @@
 #include "QuadTree.h"
 
-void QuadTree::build(const std::vector<Point>& points)
-{
+#include <stdio.h>
+#include <math.h>
 
+#define DISTANCE(x1, y1, x2, y2) \
+    (sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2)))
+
+// void QuadTree::print(int node, int indent)
+// {
+    // for (int i = 0; i < _next; i++) {
+        // printf("(%.2lf, %.2lf), m = %.2lf\n", _nodes[i].body.x,
+            // _nodes[i].body.y, _nodes[i].body.m);
+    // }
+// }
+
+const double QuadTree::k = 6.67384e-11;
+
+void QuadTree::print(int node, int indent)
+{
+    if (_nodes[node].status == Node::EMPTY) {
+        return;
+    }
+
+    for (int i = 0; i < indent; i++) {
+        printf("    ");
+    }
+
+    printf("(%.2lf, %.2lf), m = %.2lf\n", _nodes[node].body.x,
+        _nodes[node].body.y, _nodes[node].body.m);
+
+    if (_nodes[node].status == Node::INTERNAL) {
+        for (int i = 0; i < 4; i++) {
+            print(_nodes[node].children[i], indent + 1);
+        }
+    }
+}
+
+void QuadTree::build(Body *bodies)
+{
+    for (int i = 0; i < _N; i++) {
+        insert(bodies[i], 0);
+    }
 }
 
 void QuadTree::insert(const Body& body, int node)
 {
-    if (_nodes[node].leaf) { // is leaf node
-        _nodes[node].center = body.center;
-        _nodes[node].mass = body.mass;
-        _nodes[node].leaf = false;
+    if (_nodes[node].status == Node::EMPTY) { // is empty node
+// printf("node %d is empty\n", node);
+        _nodes[node].body = body;
+        _nodes[node].status = Node::EXTERNAL;
         return;
     }
 
-    if (!_nodes[node].expanded) { // is external node
+    // is internal or external node
+    Body tmp = _nodes[node].body;
+
+    // update total mass
+    _nodes[node].body.m += body.m;
+
+    // update center of mass
+    _nodes[node].body.x = (tmp.x * tmp.m + body.x * body.m) / _nodes[node].body.m;
+    _nodes[node].body.y = (tmp.y * tmp.m + body.y * body.m) / _nodes[node].body.m;
+
+    if (_nodes[node].status == Node::EXTERNAL) {
+// printf("node %d is external\n", node);
+        // expand this node
         for (int i = 0; i < 4; i++) {
-            _nodes[next].rect = _nodes[node].rect.divide(i);
             _nodes[node].children[i] = _next++;
+            int child = _nodes[node].children[i];
+            _nodes[child].status = Node::EMPTY;
+            _nodes[child].x = _nodes[node].x + (i % 2) * 0.5 * _nodes[node].w;
+            _nodes[child].y = _nodes[node].y + (i / 2) * 0.5 * _nodes[node].h;
+            _nodes[child].w = 0.5 * _nodes[node].w;
+            _nodes[child].h = 0.5 * _nodes[node].h;
+// printf("node %d: [%.2lf, %.2lf, %.2lf, %.2lf]\n", node, _nodes[node].x, _nodes[node].y, _nodes[node].w, _nodes[node].h);
+// printf("add child %d: [%.2lf, %.2lf, %.2lf, %.2lf]\n", child, _nodes[child].x, _nodes[child].y, _nodes[child].w, _nodes[child].h);
         }
-        expanded = true;
+        _nodes[node].status = Node::INTERNAL;
 
-        Body tmp;
-        tmp.center = _nodes[node].center;
-        tmp.mass = _nodes[node].mass;
-        insert(body, node);
-        insert(tmp, node);
-
-        _nodes[node].update(body);
-        _nodes[node].update(tmp);
-        return;
+        // insert body in the appropriate child
+        for (int i = 0; i < 4; i++) {
+            int child = _nodes[node].children[i];
+            if (_nodes[child].inside(tmp)) {
+                insert(tmp, child);
+                break;
+            }
+        }
+    } else {
+// printf("node %d is internal\n", node);
     }
 
-    // is internal node
-    _nodes[node].update(body);
+    // insert body in the appropriate child
     for (int i = 0; i < 4; i++) {
         int child = _nodes[node].children[i];
-        if (body.pos.inside(_nodes[child])) {
+        if (_nodes[child].inside(body)) {
             insert(body, child);
-            return;
+            break;
+        }
+    }
+}
+
+void QuadTree::search(int node, const Body& body, double& a_x, double& a_y)
+{
+    if (_nodes[node].status == Node::EXTERNAL) {
+        if (_nodes[node].body != body) {
+            double dis = DISTANCE(body.x, body.y, _nodes[node].body.x, _nodes[node].body.y);
+            double a = k * _nodes[node].body.m / (dis * dis * dis);
+            a_x += a * (_nodes[node].body.x - body.x);
+            a_y += a * (_nodes[node].body.y - body.y);
+        }
+        return;
+    }
+
+    double dis = DISTANCE(body.x, body.y, _nodes[node].body.x, _nodes[node].body.y);
+    if ((double)_size / dis < _threshold) { // treat as single body
+        double a = k * _nodes[node].body.m / (dis * dis * dis);
+        a_x += a * (_nodes[node].body.x - body.x);
+        a_y += a * (_nodes[node].body.y - body.y);
+        return;
+    }
+
+    for (int i = 0; i < 4; i++) {
+        int child = _nodes[node].children[i];
+        if (_nodes[node].status != Node::EMPTY) {
+            search(child, body, a_x, a_y);
         }
     }
 }
--- QuadTree.h
+++ QuadTree.h
@@ -1,37 +1,79 @@
 #ifndef NBODY_QUAD_TREE_H_
 #define NBODY_QUAD_TREE_H_
 
-#include "util.h"
+// #include "util.h"
 
-#define EMPTY -1
-
-struct Point
+struct Body
 {
-    double x;
-    double y;
+    int idx;
+    double x; // x-coordinate
+    double y; // y-coordinate
+    double vx; // x-velocity
+    double vy; // y-velocity
+    double m;  // mass
+
+    bool operator != (const Body& body)
+    {
+        return idx != body.idx;
+    }
 };
 
 struct Node
 {
-    Point center;
-    int size;
-    int children[4]; // index of children, -1 if null
+    int status;
+    // one of the following:
+    const static int EMPTY = 0;
+    const static int INTERNAL = 1;
+    const static int EXTERNAL = 2;
 
-    Node()
+    int children[4]; // index of children
+
+    double x, y, w, h; // border rectangle
+
+    Body body; // body
+
+    Node() { status = EMPTY; }
+
+    // body locales inside this node?
+    bool inside(const Body& body)
     {
-        children[0] = children[1] = children[2] = children[3] = EMPTY;
+        return (x < body.x && body.x < x + w) &&
+            (y < body.y && body.y < y + h);
     }
 };
 
 class QuadTree
 {
 public:
-    void build(const std::vector<Point>& points);
-    void setThreshold(double threshold);
-    double force(int body);
+    QuadTree(float threshold, double xmin, double ymin,
+        double width, double height, int N)
+        : _threshold(threshold), _N(N), _size(width)
+    {
+        _nodes = new Node[100 * N];
+        _nodes[0].x = xmin;
+        _nodes[0].y = ymin;
+        _nodes[0].w = width;
+        _nodes[0].h = height;
+        _next = 1;
+    }
+
+    void build(Body *bodies);
+
+    void search(int node, const Body& body, double& a_x, double& a_y);
+
+    void print(int node = 0, int indent = 0);
+
+    const static double k;
 
 private:
-    std::vector<Node> _nodes;
+    Node *_nodes;
+
+    int _next;
+    float _threshold;
+    int _N;
+    double _size;
+
+    void insert(const Body& body, int node);
 };
 
 #endif // NBODY_QUAD_TREE_H_
