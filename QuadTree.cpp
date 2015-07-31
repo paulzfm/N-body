0b1f212b31d67e5567da69f1277124faafffc87d
paulzfm <zhufengminpaul@163.com>
2015-07-31 23:47:54 +0800

    fix a bug: reset root as EMPTY before rebuild tree

--- QuadTree.cpp
+++ QuadTree.cpp
@@ -32,6 +32,7 @@ void QuadTree::print(int node, int indent)
 void QuadTree::build(Body *bodies)
 {
     _next = 1;
+    _nodes[0].status = Node::EMPTY;
     for (int i = 0; i < _N; i++) {
 // printf("now insert body %d...\n", bodies[i].idx);
         insert(bodies[i], 0);
