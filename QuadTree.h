58f2c4c17b66c9b03a1a8f65bd45fc2124415c63
paulzfm <zhufengminpaul@163.com>
2015-08-01 16:15:16 +0800

    debug

--- QuadTree.h
+++ QuadTree.h
@@ -20,25 +20,4 @@ struct Node
     Node() { status = EMPTY; }
 };
 
-// body locales inside this node?
-__host__ __device__ bool inside(Node *node, Body* body)
-{
-    return (node->x <= body->x && body->x <= node->x + node->w) &&
-        (node->y <= body->y && body->y <= node->y + node->h);
-}
-
-// "class" QuadTree: both __host__ and __device__
-__host__ __device__ void tree_build(Body *bodies, Node *nodes, int N, double *size);
-
-__device__ void tree_update(Body *body, Node *nodes, double size,
-    double threshold, double dt);
-
-__host__ __device__ void tree_print(Node *nodes, int node, int indent);
-
-// help functions
-__host__ __device__ void tree_insert(Body *body, int node, Node *nodes, int *next);
-
-__host__ __device__ void tree_search(int node, Body *body, double *a_x,
-    double *a_y, Node *nodes, double size, double threshold);
-
 #endif // NBODY_QUAD_TREE_H_
