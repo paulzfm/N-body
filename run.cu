00b5000c509351f68193e39f7555395163764771
paulzfm <zhufengminpaul@163.com>
2015-08-03 01:26:44 +0800

    rm assert

--- run.cu
+++ run.cu
@@ -298,9 +298,9 @@ void tree_insert(Body *body, int node, Node *nodes, int *next)
         }
     }
 
-    fprintf(stderr, "[error] body %d (%.4lf, %.4lf) isn't in any subtree of #%d [%.4lf, %.4lf, %.4lf, %.4lf]\n",
-        body->idx, body->x, body->y, node, nodes[node].x, nodes[node].y, nodes[node].w, nodes[node].h);
-    exit(1);
+    // fprintf(stderr, "[error] body %d (%.4lf, %.4lf) isn't in any subtree of #%d [%.4lf, %.4lf, %.4lf, %.4lf]\n",
+        // body->idx, body->x, body->y, node, nodes[node].x, nodes[node].y, nodes[node].w, nodes[node].h);
+    // exit(1);
 }
 
 
